b439b8717dbfff6de41a91877288f27a69f32c8f
rhagen87 <reynold.hagen@gmail.com>
2020-07-28 16:37:17 -0500

    Initial Commit

---
+++ Data/GameDBContext.cs
@@ -0,0 +1,16 @@
+﻿using GameCatalog.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameCatalog.Data
+{
+    public class GameDBContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+
+        public GameDBContext(DbContextOptions<GameDBContext> options) : base(options) { }
+    }
+}
