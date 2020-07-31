11e71f251e840b3ac9eeef9b5300a2d291215624
rhagen87 <reynold.hagen@gmail.com>
2020-07-31 13:49:50 -0500

    filled game class

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GameCatalog.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GameCatalog.Controllers
 {
     public class GameController : Controller
     {
+        private GameDBContext context;
+
+        public GameController(GameDBContext dBContext)
+        {
+            context = dBContext;
+        }
         public IActionResult Index()
         {
             return View();
--- Models/Game.cs
+++ Models/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +8,29 @@ namespace GameCatalog.Models
 {
     public class Game
     {
-        public string Name { get; set; }
+
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Cover { get; set; }
+        public float Rating { get; set; }
+        public int GenreId { get; set; }
+        public int DeveloperId { get; set; }
 
         public Game() { }
 
-        public Game(string name)
+        public Game(string name, string description, DateTime releaseDate, string cover, float rating, int genreId, int developerId)
         {
             Name = name;
+            Description = description;
+            ReleaseDate = releaseDate;
+            Cover = cover;
+            Rating = rating;
+            GenreId = genreId;
+            DeveloperId = developerId;
+
         }
     }
 }
